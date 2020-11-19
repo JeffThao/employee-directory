567885d4b944626d63224684d052ea293385122e
Jeff T <jeff.thao92@gmail.com>
2020-11-19 00:28:33 -0600

    installed npm axios and bootstrap. Changed the title in index.html to Employee Directory. Created class App in app.js. Use API.searchEmployee to get api data.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Section from "./components/Section";
+import Header from "./components/Header";
+import API from "./utils/API";
+// import FriendCard from "./components/FriendCard";
+// import Wrapper from "./components/Wrapper";
+// import Title from "./components/Title";
+// import friends from "./friends.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    employee:[]
+  };
+
+  componentDidMount() {
+    API.searchEmployee()
+    .then(data =>
+      { 
+      this.setState({ employee: data.data.results })
+      console.log({ employee: data.data.results });
+     this.sortPhone();
+     this.sortEmail();
+    }
+    )
+    
+    // .then(function(data){
+    //     this.setState({ this.state.employee : data.data.results})
+    //   console.log(data.data.results)
+    // });
+  }
+
+  sortEmail =()  => {
+    console.log(this.state.employee.sort(function(a, b) {
+      var nameA = a.email.toUpperCase(); // ignore upper and lowercase
+      var nameB = b.email.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+    
+      // names must be equal
+      return 0;
+    }));
+    // // Set this.state.friends equal to the new friends array
+    // this.setState({ friends });
+  };
+
+  sortPhone =()  => {
+    console.log(this.state.employee.sort(function (a, b) {
+      return a.cell - b.cell;
+    }));
+  
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Section
+       employee = {this.state.employee}
+        />
+      {/* <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper> */}
+      </div>
+    );
+  }
 }
 
 export default App;
