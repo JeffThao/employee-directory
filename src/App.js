b884905d2074f93bd5df47e4ef315e3ea78605c7
Jeff T <jeff.thao92@gmail.com>
2020-11-19 16:48:19 -0600

    Added sortName function to sort by name.

--- src/App.js
+++ src/App.js
@@ -14,26 +14,37 @@ class App extends Component {
       { 
       this.setState({ employee: data.data.results })
       console.log({ employee: data.data.results });
+      this.sortName();
       this.sortPhone();
       this.sortEmail();
-    }
-    )
-    
-    // .then(function(data){
-    //     this.setState({ this.state.employee : data.data.results})
-    //   console.log(data.data.results)
-    // });
+    })
   }
 
-
-  sortEmail =()  => {
+  sortName =()  => {
     console.log(this.state.employee.sort(function(a, b) {
-      var nameA = a.email.toUpperCase(); // ignore upper and lowercase
-      var nameB = b.email.toUpperCase(); // ignore upper and lowercase
+      // ignore upper and lowercase
+      let nameA = a.name;
+      let nameB = b.name;
       if (nameA < nameB) {
         return -1;
       }
       if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    }));
+
+  };
+
+  sortEmail =()  => {
+    console.log(this.state.employee.sort(function(a, b) {
+      // ignore upper and lowercase
+      let emailA = a.email.toUpperCase();
+      let emailB = b.email.toUpperCase();
+      if (emailA < emailB) {
+        return -1;
+      }
+      if (emailA > emailB) {
         return 1;
       }
       return 0;
