5f32f4000d7583209912b74641985d5083391982
Jeff T <jeff.thao92@gmail.com>
2020-11-19 15:59:57 -0600

    Added import Section and Header. Created sortEmail and sortPhone to be able to sort by email and phone. Added the components for Header and Section on the return at the end. Also included 'employee = {this.state.employee}' into the Section so that it can use and call this.state.employee from app.js.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import React, { Component } from "react";
 import Section from "./components/Section";
 import Header from "./components/Header";
 import API from "./utils/API";
-// import FriendCard from "./components/FriendCard";
-// import Wrapper from "./components/Wrapper";
-// import Title from "./components/Title";
-// import friends from "./friends.json";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
   state = {
     employee:[]
   };
@@ -19,8 +14,8 @@ class App extends Component {
       { 
       this.setState({ employee: data.data.results })
       console.log({ employee: data.data.results });
-     this.sortPhone();
-     this.sortEmail();
+      this.sortPhone();
+      this.sortEmail();
     }
     )
     
@@ -29,6 +24,7 @@ class App extends Component {
     //   console.log(data.data.results)
     // });
   }
+
 
   sortEmail =()  => {
     console.log(this.state.employee.sort(function(a, b) {
@@ -40,12 +36,9 @@ class App extends Component {
       if (nameA > nameB) {
         return 1;
       }
-    
-      // names must be equal
       return 0;
     }));
-    // // Set this.state.friends equal to the new friends array
-    // this.setState({ friends });
+
   };
 
   sortPhone =()  => {
@@ -55,7 +48,6 @@ class App extends Component {
   
   };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <div>
@@ -63,20 +55,6 @@ class App extends Component {
         <Section
        employee = {this.state.employee}
         />
-      {/* <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper> */}
       </div>
     );
   }
